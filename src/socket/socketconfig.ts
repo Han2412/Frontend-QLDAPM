a8daebb327c935592f7d34eeb89274e37d0799be
xuabcuong <xuabcuong@gmail.com>
2025-12-26 17:23:52 +0700

    add

--- src/socket/socketconfig.ts
+++ src/socket/socketconfig.ts
@@ -1,3 +1,5 @@
 import ioClient from "socket.io-client";
 
-export const socket = ioClient("https://qldapm-qlcf-be.onrender.com");
+export const socket = ioClient("https://qldapm-qlcf-be.onrender.com", {
+  transports: ["websocket"],
+});
